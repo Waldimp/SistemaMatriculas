f1161f9278ff68a73f1276f6eb86e72a834875a3
giova12344 <giovannimontes45@gmail.com>
2020-08-31 23:49:17 -0600

    funcionalidadnpe

--- src/app/components/validarnpe/validarnpe.component.ts
+++ src/app/components/validarnpe/validarnpe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-validarnpe',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidarnpeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+
+
+
+  onValidateNPE(npeForm:NgForm): void{
+    this.onLogin();
+  }
+
+
+
+
+
+  onLogin(): void{
+    this.router.navigate(['']);
   }
 
 }
